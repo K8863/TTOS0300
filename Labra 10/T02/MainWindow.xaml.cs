7ea1a524deda01e066e1cc82e36bf55728040ee0
Niia <Niia@PC>
2017-02-08 16:20:17 +0200

    Initial commit

--- Labra 10/T02/MainWindow.xaml.cs
+++ Labra 10/T02/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ using System.Windows.Shapes;
 namespace T02
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Tee sovellus, jossa käyttäjältä voidaan kysyä käyttäjätunnus ja salasana. Tavoitteena on oppia Grid-kontrollin käyttöä eli älä sijoita kontrolleja Margin-määreiden avulla vapaasti, vaan käytä layoutin suunnittelussa apuna Grid-, GridDefinitions-, RowDefinitions-, ColumnDefinitions-määreitä. Keskitä muodostamasi Grid keskelle näyttölaitetta. Tulosta käyttäjätunnus ja salasana Visual Studion Output-ikkunaan (vihje: Debug.WriteLine();) sen jälkeen kun Login-painiketta on painettu.
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -25,10 +26,9 @@ namespace T02
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            //txbHello.Text = "Hello " + txtName.Text;
-
+            Debug.WriteLine(txbLogin.Text + " " + txtLogin.Text + " " + txbPsswd.Text + " " + txtPsswd.Text);
         }
     }
 }
