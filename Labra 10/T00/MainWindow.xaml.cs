dbcf536f698b27794d9f21f043103ee98e786fd5
K8863 <K8863@ICT-421-WS-35>
2017-02-08 14:34:38 +0200

    Initial commit

---
+++ Labra 10/T00/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T00
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            txbHello.Text = "Hello " + txtName.Text;
+            MessageBox.Show("Terve " + txtName.Text, "Messuboksi");
+            txbMessages.Text = "Painoit buttosta btnSayHello";
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            // Kutsutaan näkyviin About-niminen ikkuna
+            About aboutWin = new About();
+            // Huom! Ikkuna voi olla joko modaalinen tai tavallinen
+            //aboutWin.ShowDialog();  // Modaalinen
+            aboutWin.Show();
+        }
+    }
+}
---
+++ Labra 10/T01/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string checkedBoxes;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Check(CheckBox chb)
+        {
+            if ((bool)chb.IsChecked)
+            {
+                checkedBoxes += chb.Content.ToString() + " ";
+            }
+            txtList.Text = checkedBoxes;
+        }
+        private void btnBuy_Click(object sender, RoutedEventArgs e)
+        {
+            Check(chbBeer);
+            Check(chbCoke);
+            Check(chbPizza);
+            Check(chbChocolate);
+            checkedBoxes = "";
+        }
+    }
+}
