13c4d0acc2bd7fe191c355ae4c4d1f34c36c1d1d
K8863 <K8863@ICT-421-WS-35>
2017-02-13 13:49:42 +0200

    Initial commit

---
+++ Labra 11/WPF_Demo_X3/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.ICT;
+
+namespace WPF_Demo_X3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Koska useampi metodi-tapahtumakäsittelijä tulee käyttämään näitä muuttujia -->
+        // määritellään luokan tasolle luokan jäsenmuuttujiksi
+        JAMK.ICT.HockeyLeague liiga;
+        ObservableCollection<JAMK.ICT.HockeyTeam> joukkueet;
+        int counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IniMyStuff();
+        }
+        private void IniMyStuff()
+        {
+            // Tänne kootusti omien kontrollien alustukset
+            List<string> muuvit = new List<string>();
+            muuvit.Add("Halloween");
+            muuvit.Add("Jaws");
+            muuvit.Add("Star Wars");
+            cmbMovies.ItemsSource = muuvit;
+
+            // Haetaan SM-liigajoukkueet
+            liiga = new JAMK.ICT.HockeyLeague();       
+            joukkueet = liiga.GetTeams();
+            cmbTeams.ItemsSource = joukkueet;
+        }
+
+        private void btnBind_Click(object sender, RoutedEventArgs e)
+        {
+            // Määritellään StackPanelin DataContext
+            // Demo1: datakontekstini on olio
+            //HockeyTeam tiimi = new HockeyTeam("Tappara", "Tampere");
+            //spRight.DataContext = tiimi;
+
+            // Demo2: Kytketään olio-kokoelman 1. olioon
+            spRight.DataContext = joukkueet[counter];
+        }
+    }
+}
